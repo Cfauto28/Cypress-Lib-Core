3a52192f61dfd92e81bad722986b97fb2b7e969d
Cfauto <cfautoeclasse5a@gmail.com>
2026-07-30 13:34:31 +0200

    Better fix for sound

--- src/main/java/net/cfauto/cypress_lib_core/ThreadDownloadResourcesFix.java
+++ src/main/java/net/cfauto/cypress_lib_core/ThreadDownloadResourcesFix.java
@@ -10,7 +10,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class ThreadDownloadResourcesFix extends SimpleFileVisitor<Path> {
-	protected ThreadDownloadResourcesFix() {
+	public ThreadDownloadResourcesFix() {
 	}
 
 	public @NotNull FileVisitResult visitFile(@NotNull Path path, @NotNull BasicFileAttributes basicFileAttributes) throws IOException {
--- src/main/java/net/cfauto/cypress_lib_core/mixin/ResourceDownloaderMixin.java
+++ src/main/java/net/cfauto/cypress_lib_core/mixin/ResourceDownloaderMixin.java
@@ -1,46 +1,24 @@
 package net.cfauto.cypress_lib_core.mixin;
 
+import net.cfauto.cypress_lib_core.ThreadDownloadResourcesFix;
 import net.minecraft.client.resource.ResourceDownloader;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 
-import net.cfauto.cypress_lib_core.ThreadDownloadResourcesFix;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
+import java.nio.file.FileVisitor;
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
 
 @Mixin(ResourceDownloader.class)
 public class ResourceDownloaderMixin {
-	/**
-	 * @author Cfauto
-	 * @reason Fix ExtSounds
-	 */
-	@Overwrite
-	public void method_1_2275(Path path) throws IOException {
-		ThreadDownloadResourcesFix threadDownloadResourcesUnknownClass1 = new ThreadDownloadResourcesFix() {
-			public @NotNull FileVisitResult visitFile(@NotNull Path path, @NotNull BasicFileAttributes basicFileAttributes) throws IOException {
-				Files.delete(path);
-				return FileVisitResult.CONTINUE;
-			}
+	@Unique
+	ThreadDownloadResourcesFix threadDownloadResourcesUnknownClass1 = new ThreadDownloadResourcesFix();
 
-			public @NotNull FileVisitResult visitFileFailed(@NotNull Path path, @NotNull IOException iOException) throws IOException {
-				Files.delete(path);
-				return FileVisitResult.CONTINUE;
-			}
-
-			public @NotNull FileVisitResult postVisitDirectory(@NotNull Path path, IOException iOException) throws IOException {
-				if (iOException != null) {
-					throw iOException;
-				}
-
-				Files.delete(path);
-				return FileVisitResult.CONTINUE;
-			}
-		};
-		Files.walkFileTree(path, threadDownloadResourcesUnknownClass1);
+	@ModifyArg(method = "method_1_2275", at = @At(value = "INVOKE", target = "Ljava/nio/file/Files;walkFileTree(Ljava/nio/file/Path;Ljava/nio/file/FileVisitor;)Ljava/nio/file/Path;"), index = 1)
+	private FileVisitor<? super Path> mixin(FileVisitor<? super @NotNull Path> visitor) {
+		return threadDownloadResourcesUnknownClass1;
 	}
 }
