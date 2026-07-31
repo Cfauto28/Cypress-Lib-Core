0341e11d0e41ccd88650ff89a2a66ec33878020d
Cfauto <cfautoeclasse5a@gmail.com>
2026-07-31 12:03:30 +0200

    Revert to the invasive mixin

--- src/main/java/net/cfauto/cypress_lib_core/mixin/ResourceDownloaderMixin.java
+++ src/main/java/net/cfauto/cypress_lib_core/mixin/ResourceDownloaderMixin.java
@@ -5,20 +5,45 @@ import net.minecraft.client.resource.ResourceDownloader;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import java.nio.file.FileVisitor;
-import java.nio.file.Path;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 @Mixin(ResourceDownloader.class)
 public class ResourceDownloaderMixin {
-	@Unique
-	ThreadDownloadResourcesFix threadDownloadResourcesUnknownClass1 = new ThreadDownloadResourcesFix();
 
-	@ModifyArg(method = "method_1_2275", at = @At(value = "INVOKE", target = "Ljava/nio/file/Files;walkFileTree(Ljava/nio/file/Path;Ljava/nio/file/FileVisitor;)Ljava/nio/file/Path;"), index = 1)
-	private FileVisitor<? super Path> mixin(FileVisitor<? super @NotNull Path> visitor) {
-		return threadDownloadResourcesUnknownClass1;
+	/**
+	 * @author Cfauto
+	 * @reason Fix sounds and resource extraction
+	 */
+	@Overwrite
+	public void method_1_2275(Path path) throws IOException {
+		ThreadDownloadResourcesFix threadDownloadResourcesUnknownClass1 = new ThreadDownloadResourcesFix() {
+			public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+				Files.delete(path);
+				return FileVisitResult.CONTINUE;
+			}
+
+			public FileVisitResult visitFileFailed(Path path, IOException iOException) throws IOException {
+				Files.delete(path);
+				return FileVisitResult.CONTINUE;
+			}
+
+			public FileVisitResult postVisitDirectory(Path path, IOException iOException) throws IOException {
+				if (iOException != null) {
+					throw iOException;
+				}
+
+				Files.delete(path);
+				return FileVisitResult.CONTINUE;
+			}
+		};
+		Files.walkFileTree(path, threadDownloadResourcesUnknownClass1);
 	}
+
 }
